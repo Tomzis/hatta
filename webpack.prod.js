b1b99e950048d169b4831661ba7fdcb6192d350a
Tomasz <tometk2005@gmail.com>
2023-03-28 00:21:21 +0200

    add img and favicon folder to config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -20,11 +21,29 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ filename: "index.html", template: "./index.html" }),
-    new HtmlWebpackPlugin({ filename: "./about/index.html", template: "./about/index.html" }),
-    new HtmlWebpackPlugin({ filename: "./articles/index.html", template: "./articles/index.html" }),
-    new HtmlWebpackPlugin({ filename: "./gallery/index.html", template: "./gallery/index.html" }),
-    new HtmlWebpackPlugin({ filename: "./contact/index.html", template: "./contact/index.html" }),
+    new HtmlWebpackPlugin({
+      filename: "./about/index.html",
+      template: "./about/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "./articles/index.html",
+      template: "./articles/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "./gallery/index.html",
+      template: "./gallery/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "./contact/index.html",
+      template: "./contact/index.html",
+    }),
     new HtmlWebpackPlugin({ filename: "error.html", template: "./error.html" }),
     new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+    new CopyPlugin({
+      patterns: [
+        { from: "./favicon", to: "favicon" },
+        { from: "./src/img", to: "img" },
+      ],
+    }),
   ],
 };
